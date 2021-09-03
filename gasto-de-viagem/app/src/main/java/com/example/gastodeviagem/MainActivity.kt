b45d872b6a890a85664e143fa6275cd462b7a1ee
Germano Lira <dolan.3@hotmail.com>
2021-09-03 15:41:12 -0300

    lógica

--- gasto-de-viagem/app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ gasto-de-viagem/app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gastodeviagem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -21,13 +22,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun calculate() {
+        if(validationOK()) {
 
-        val distance = editDistance.text.toString().toFloat()
-        val price = editPrice.text.toString().toFloat()
-        val autonomy = editAutonomy.text.toString().toFloat()
+            val distance = editDistance.text.toString().toFloat()
+            val price = editPrice.text.toString().toFloat()
+            val autonomy = editAutonomy.text.toString().toFloat()
 
-        val totalValue =(distance * price) / autonomy
-        textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+            val totalValue = (distance * price) / autonomy
+            textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        }
+    }
 
+    private fun validationOK() : Boolean {
+        return (editDistance.text.toString() != ""
+                && editPrice.text.toString() != ""
+                && editAutonomy.text.toString() != "")
     }
 }
